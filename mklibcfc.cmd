776767782b1d3c5fc980364df48831157230e820
David Rogers <davidwrogers2@gmail.com>
2023-07-22 11:16:11 -0500

    misc lib gen stuff

--- mklibcfc.cmd
+++ mklibcfc.cmd
@@ -1,2 +1,3 @@
+REM todo: generate NodeKind -> int for java interop
 mkdir out 2> NUL
-npx esbuild src/libcfc/libcfc.ts --bundle --target=esnext --format=esm --outfile=out/libcfc.js
+npx esbuild src/libcfc/libcfc.ts --bundle --target=esnext --format=iife --global-name=libcfc --outfile=out/libcfc.js
--- src/libcfc/libcfc.ts
+++ src/libcfc/libcfc.ts
@@ -1,1 +1,35 @@
-export { Parser } from "../compiler/parser"
+import { Parser } from "../compiler/parser"
+import { SourceFile, type Node } from "../compiler/node"
+import { EngineVersions } from "../compiler/engines";
+import { CfFileType } from "../compiler/scanner";
+
+export function parse(kind: string, src: string) {
+    if (kind !== "cfm" && kind !== "cfc") {
+        throw Error(`parse() called with kind '${kind}', expected either 'cfm' or 'cfc'`);
+    }
+
+    const file = SourceFile("", kind === "cfm" ? CfFileType.cfm : CfFileType.cfc, src);
+
+    const dummyConfig = {
+        debug: false,
+        parseTypes: false,
+        engineVersion: EngineVersions["lucee.5"],
+        withWireboxResolution: false,
+        cfConfigProjectRelativePath: null,
+        genericFunctionInference: false,
+        checkReturnTypes: false,
+        checkFlowTypes: false,
+        cancellationToken: {
+            cancellationRequested: () => false,
+            throwIfCancellationRequested: () => {}
+        },
+    };
+    
+    Parser(dummyConfig).parse(file);
+    
+    return {
+        hasErrors: file.diagnostics.length > 0,
+        errors: file.diagnostics,
+        sourceFile: file
+    }
+}
